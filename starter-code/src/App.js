f5af7b77d594b018283f0fdfce5617303c2dbd9a
Lawlieet <juan.1514@hotmail.com>
2020-02-20 01:06:58 -0600

    Solo Banderas

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
+import countries from './countries.json'
+import Detail from './components/Detail'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div >
+        <h1 className="h1">Wiki-Countries</h1>
+        <div>
+            <div> 
+            {/* le hacemos un mal al json el cual pasara a tener 2 parametros 1 donde se guardara y el index */}
+            {/* Recordar diego menciono que se necesita poner el key al padre anterior al link algo asi */}
+            {countries.map((country, index) => 
+             
+              <div key={index}>
+                <Link  to={`/${country.cca3}`}>{country.flag} {country.name.common}</Link>
+              </div>
+            )}
+        </div>
+            <div>
+            <Route exact path="/:id" component={Detail} />
+            </div>  
+          </div>
+          
+        </div>
+      
+    );
+  }
 }
 
 export default App;
---
+++ starter-code/src/components/Detail.js
@@ -0,0 +1,15 @@
+//https://medium.com/@dvenegas/empezando-con-react-router-v4-2b8c31c7eb11
+import React, {Component} from 'react'
+import data from '../countries.json'
+import {Link} from 'react-router-dom'
+
+
+const Detail = (props) =>{
+
+    const country = (cca3) => {}
+
+    const { params } = props.match
+
+}
+
+export default Detail
